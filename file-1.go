54bf04580e7d7b6b2c38a3f8045cb11950febe4b
petez7519 <127163326+petez7519@users.noreply.github.com>
2024-09-19 10:44:34 -0700

    Summary
    Stuff

--- file-1.go
+++ file-1.go
@@ -4,9 +4,9 @@ import "fmt"
 
 // Multiply multiplies two integers and returns the result.
 func Multiply(a, b int) int {
-    return a * b
+	return a * b
 }
 
 func main() {
-    fmt.Println("Multiplication:", Multiply(2, 3))
+	fmt.Println("Multiplication:", Multiply(2, 3))
 }
